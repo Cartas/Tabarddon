f4d6cb287fc43b1282a1c4cee242e45766a2d6da
Richard Livesey <Livesey7@hotmail.co.uk>
2011-12-20 06:56:01 +0100

    Better aligned the listing somewhat.

--- vo/FactionsTabards.lua
+++ vo/FactionsTabards.lua
@@ -5,30 +5,30 @@ ns.guilds = {
     69210   -- Guild 100%
 }
 
+-- {factionID, tabardID}
 ns.factions = {
+    {1133,  64884}, -- Bilgewater Cartel
+    {530,   45582}, -- Darkspear Trolls
+    {69,    45579}, -- Darnassus
+    {930,   45580}, -- Exodar
+    {1134,  64882}, -- Gilneas
+    {54,    45578}, -- Gnomeregan
+    {47,    45577}, -- Ironforge
+    {76,    45581}, -- Orgrimmar
+    {911,   45585}, -- Silvermoon City
+    {72,    45574}, -- Stormwind
+    {81,    45584}, -- Thunder Bluff
+    {68,    45583}, -- Undercity
 
-    {1133, 64884}, -- Bilgewater Cartel
-    {530, 45582},  -- Darkspear Trolls
-    {69, 45579},   -- Darnassus
-    {930, 45580},  -- Exodar
-    {1134, 64882}, -- Gilneas
-    {54, 45578},   -- Gnomeregan
-    {47, 45577},   -- Ironforge
-    {76, 45581},   -- Orgrimmar
-    {911, 45585},  -- Silvermoon City
-    {72, 45574},   -- Stormwind
-    {81, 45584},   -- Thunder Bluff
-    {68, 45583},   -- Undercity
+    {1106,  43154}, -- Argent Crusade
+    {1098,  43155}, -- Knights of the Ebon Blade
+    {1090,  43157}, -- Kirin Tor
+    {1091,  43156}, -- The Wyrmrest Accord
 
-    {1106, 43154}, -- Argent Crusade
-    {1098, 43155}, -- Knights of the Ebon Blade
-    {1090, 43157}, -- Kirin Tor
-    {1091, 43156}, -- The Wyrmrest Accord
-
-    {1172, 65909},  -- Dragonmaw Clan
-    {1158, 65906}, -- Guardians of Hyjal
-    {1173, 65904}, -- Ramkahen
-    {1171, 65907}, -- Therazane
-    {1135, 65905}, -- The Earthen Ring
-    {1174, 65908}  -- Wildhammer Clan
+    {1172,  65909}, -- Dragonmaw Clan
+    {1158,  65906}, -- Guardians of Hyjal
+    {1173,  65904}, -- Ramkahen
+    {1171,  65907}, -- Therazane
+    {1135,  65905}, -- The Earthen Ring
+    {1174,  65908}  -- Wildhammer Clan
 } 
